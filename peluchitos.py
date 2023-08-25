ccb1325f0e0a7ad001ab057de2f53b1ba0ade41a
SEBAS <js.sierramunerajuansebastian999@gmail.com>
2023-08-25 11:16:46 -0500

    nt4 Se hacen ejemplo de siclo while y una lista de opciones para una tienda llamada pelucheria

---
+++ peluchitos.py
@@ -0,0 +1,29 @@
+peluches=["roshi","conejito","aguacatico"]
+opcion=0
+
+print("Pelucheria PELUCHITOS")
+print(".....................")
+print("1. Agregar Producto a la bodega")
+print("2. Ver productos en bodega")
+print("3. Obtener del inventario")
+print("4. Ver productos mas vendidos")
+print("5. SALIR")
+
+while(opcion!=5):
+    opcion=opcion+1
+    opcion=int(input("digita un numero: "))
+    if opcion==1:
+        print("Usted esta en la opción 1")
+    elif opcion==2:
+        print("Usted esta en la opción 2")
+    elif opcion==3:
+        print("Usted esta en la opción 3")
+    elif opcion==4:
+        print("Usted esta en la opción 4")
+    elif opcion==5:
+        print("Chao")
+    else:
+        print("Valor no valido")
+print("sali del siclo")
+
+    
